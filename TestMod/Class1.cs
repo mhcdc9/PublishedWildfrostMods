cf2df021b5d55e5b84c725298bda4a16b1656f81
Michael Coopman <mhcoopman@gmail.com>
2024-04-21 01:50:42 -0400

    Testing something.

---
+++ TestMod/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMod
+{
+    internal class StatusEffectChangeData : StatusEffectData
+    {
+        public CardData cardBase;
+        public int keepIndex = 0;
+
+        public override void Init()
+        {
+
+            base.OnTurn += TurnStart;
+        }
+
+        private IEnumerator TurnStart(Entity entity)
+        {
+            if (entity == target)
+            {
+                ChangeCard();
+                target.PromptUpdate();
+                target.display.UpdateData(true);
+            }
+            yield break;
+        }
+
+        public void ChangeCard()
+        {
+            IEnumerable<CardData> cards = AddressableLoader.GetGroup<CardData>("CardData").InRandomOrder();
+            foreach(CardData card in cards)
+            {
+                if (card.cardType.name == "Item")
+                {
+                    cardBase = card;
+                    break;
+                }
+            }
+        }
+
+        public void UpdateData()
+        {
+            CardData trueData = target.data;
+            trueData.mainSprite = cardBase.mainSprite;
+            trueData.canPlayOnBoard = cardBase.canPlayOnBoard;
+            trueData.canPlayOnEnemy = cardBase.canPlayOnEnemy;
+            trueData.canPlayOnHand = cardBase.canPlayOnHand;
+            trueData.canPlayOnFriendly = cardBase.canPlayOnFriendly;
+            trueData.damage = cardBase.damage;
+            trueData.needsTarget = cardBase.needsTarget;
+            trueData.playOnSlot = cardBase.playOnSlot;
+            trueData.textInsert = cardBase.title;
+
+            trueData.startWithEffects = CardData.StatusEffectStacks.Stack(new CardData.StatusEffectStacks[1] { trueData.startWithEffects[keepIndex] }, cardBase.startWithEffects);
+            trueData.attackEffects = cardBase.attackEffects;
+        }
+    }
+}
